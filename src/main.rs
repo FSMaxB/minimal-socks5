0eb9572b11da0f1d8f513cd01b005dfa2b66a25c
Max Bruckner <max@maxbruckner.de>
2022-07-26 22:24:36 +0200

    Initial cli argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 //! https://datatracker.ietf.org/doc/html/rfc1928
-fn main() {
-	println!("Hello, world!");
+
+use clap::Parser;
+use std::net::IpAddr;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+	let parameters = Parameters::parse();
+
+	println!("{parameters:?}");
+	Ok(())
 }
 
+#[derive(Debug, Parser)]
+struct Parameters {
+	#[clap(long)]
+	socks_address: IpAddr,
+	#[clap(long, default_value = "1080")]
+	socks_port: u16,
+}
 
 mod message;
