6404309813cda7f96486c9e3c0339ed3c99ba699
Max Bruckner <max@maxbruckner.de>
2022-08-01 09:07:11 +0200

    Shut down on receiving Ctrl-C

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,33 @@ use crate::server::listen_for_tcp_connections;
 use anyhow::Context;
 use clap::Parser;
 use std::net::{IpAddr, ToSocketAddrs};
+use tokio::sync::oneshot;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
 	let Parameters { address, port } = Parameters::parse();
 
+	let (shutdown_sender, shutdown_receiver) = oneshot::channel();
+	ctrlc::set_handler({
+		let mut shutdown_sender = Some(shutdown_sender);
+		move || {
+			let _ = shutdown_sender.take().map(|sender| sender.send(()));
+		}
+	})
+	.context("Failed to register Ctrl-C handler")?;
+
 	let socket_address = (address, port)
 		.to_socket_addrs()?
 		.next()
 		.with_context(|| format!("Invalid Ip/port: {address}:{port}"))?;
-	listen_for_tcp_connections(socket_address).await?;
+	tokio::select! {
+		result = listen_for_tcp_connections(socket_address) => {
+			result?;
+		}
+		_ = shutdown_receiver => {
+			println!("Received ctrl-c, shutting down")
+		}
+	};
 
 	Ok(())
 }
